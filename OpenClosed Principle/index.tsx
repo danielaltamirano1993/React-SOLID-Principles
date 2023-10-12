f23c1d1a19626cb33549af668666bf88172e0316
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-10-12 12:13:18 -0500

    a4

--- OpenClosed Principle/index.tsx
+++ OpenClosed Principle/index.tsx
@@ -1,23 +1,40 @@
-import { FC } from "react";
+import { FC, ReactElement } from "react";
 
 type Props = {
   title: string;
   type: "default" | "withLinkButton" | "withNormalButton";
-  href?: "string";
+  href?: string;
   buttonText?: string;
   onClick?: () => void;
 };
 
-const Title: FC<Props> = ({
-  title,
-  type,
-  href,
-  buttonText,
-  onClick,
-}) => {
-  return (
-    <div style={{ display: "flex", justifyContent: "space-between" }}>
+type TitleProps = {
+  children: ReactElement
+  title: string
+}
 
-    </div>
+const Title: FC<TitleProps> = ({ title, children }) => {
+  return (<div style={{ display: "flex", justifyContent: "space-between"}}>
+    <h1>{title}</h1>
+    {children}
+  </div>
   )
 }
+
+type TitleWithLinkProps = {
+  title: string,
+  href: string,
+  buttonText: string
+}
+
+export const TitleWithLink: FC<TitleWithLinkProps> = ({ title, href, buttonText }) => {
+  return (
+    <Title title = {title}>
+      <div>
+         <a href={href}>{buttonText}</a> 
+        </div>  
+    </Title>
+  )
+}
+
+
